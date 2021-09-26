573109089eed0e67b391b9bf9c750be6680222af
Vadim Ershov <vadim.ershov.01@mail.ru>
2021-09-26 08:08:35 +0300

    Initial commit

---
+++ src/main/java/ru/divyatyybe/hakaton_tsifra/HakatonTsifraApplication.java
@@ -0,0 +1,13 @@
+package ru.divyatyybe.hakaton_tsifra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakatonTsifraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HakatonTsifraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/divyatyybe/hakaton_tsifra/HakatonTsifraApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.divyatyybe.hakaton_tsifra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakatonTsifraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
